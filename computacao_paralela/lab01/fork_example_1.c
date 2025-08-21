26f17de21250581dfee55c2bb6170d0bbed51489
Pedro Nogueira <73959558+PedroNogueirar@users.noreply.github.com>
2025-08-21 23:08:51 +0000

    Adiciona fork_example_1

---
+++ computacao_paralela/lab01/fork_example_1.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if(pid < 0) {
+        fprintf(stderr , " Fork falhou !\n");
+        return 1;
+    }
+    else if (pid == 0) {
+        printf("Eu sou o filho!\n");
+    } 
+    else {
+        printf("Eu sou o pai!\n");
+        wait(NULL);
+        printf("Meu filho terminou , agora eu posso terminar.\n");
+    }
+    return 0;
+}
